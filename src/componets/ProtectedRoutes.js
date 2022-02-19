b1ebc06d3e1edc32d3817b3833ef2f51df1f38a9
Amurdock6 <77793000+Amurdock6@users.noreply.github.com>
2022-02-19 15:10:38 -0700

    ProtectedRoute Updates

--- src/componets/ProtectedRoutes.js
+++ src/componets/ProtectedRoutes.js
@@ -1,20 +1,38 @@
 import axios from 'axios'
 import { Outlet, Navigate } from "react-router";
 
-let auth = false
+// axios.post('http://localhost:5000/middleware/protectedroutes', {
+//     cookieValue: cookieValue
+// }).then(sendValue => {
+//     return axios.get('http://localhost:5000/auth', { withCredentials: true }).then((res) => {
+//         // console.log(res.data)
+//     })
+// });
 
-axios.get('http://localhost:5000/auth', { withCredentials: true }).then((res) => {
-        auth = ''
-        // figure out how to set auth equal to true based on the middleWare auth on the backend
-    });
 const useAuth = () => {
-    const authorized = {loggedIn: false}
-    return authorized && authorized.loggedIn;
-}
+    const cookieValue = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('auth='))
+        .split('=')[1];
+
+    if ( cookieValue === 'true') {
+        const authorized = {loggedIn: true}
+        return authorized && authorized.loggedIn;
+    } else {
+        const authorized = {loggedIn: false}
+        return authorized && authorized.loggedIn;
+    }
+};
 
 const ProtectedRoutes = () => {
-        const isAuth = useAuth();
+    const isAuth = useAuth();
     return isAuth ? <Outlet/> : <Navigate to="/login" />;
 }
 
 export default ProtectedRoutes
+
+// Need to assign the auth cookie with the user on login as well as the user id and send it with the httpOnly token on login/registration
+// Then keep this page as is just change up the value of auth to be a random string so instead of true or false make it a ran string val 
+// That i will first send to the front end from backend when logedin/registration
+// Then send it to backend to verify that value is correct and not something a user added
+// this should make protected route more secure
--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,6 +1,9 @@
+import NavBar from "../componets/NavBar/NavBar"
+
 function Account() {
     return (
         <div>
+            <NavBar />
             <h1>Account Page</h1>
         </div>
     )
