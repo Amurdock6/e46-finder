845083978ad7589f0d07fd26fa841313ea354746
Amurdock6 <77793000+Amurdock6@users.noreply.github.com>
2022-06-06 00:44:43 -0600

    added errors for password reset

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -78,7 +78,7 @@ const ForgotPassword = () => {
                     </div>
 
                     <div id='arrow-wrapper'>
-                        <Link to='/confirmation'>
+                        <Link to='/login'>
                             <FontAwesomeIcon icon={faArrowLeft} /><span>Back to login</span>
                         </Link>
                     </div>
